92ef02fc1d3c4f9d16702e0b117ace1d78466189
anwarsaepul <saepulanwarxyz@gmail.com>
2021-05-26 08:31:10 +0700

    memecah dashboard menjadi 4 bagian

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -2,7 +2,9 @@
 
 class Dashboard extends CI_Controller{
     function index(){
+        $this->load->view('templates/header');
+        $this->load->view('templates/sidebar');
         $this->load->view('dashboard');
-
+        $this->load->view('templates/footer');
     }
 }
